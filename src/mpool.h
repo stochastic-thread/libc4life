58a8e5bbaf3ccb8cb790d0090753774323f598c8
codr4life <codr4life@gmail.com>
2016-11-12 05:25:36 +0100

    cleaning

--- src/mpool.h
+++ src/mpool.h
@@ -8,14 +8,7 @@
   struct c4mpool var;				\
   c4mpool_init(&var);				\
 
-#define C4MPOOL_ALLOC(mpool, type, cnt)		\
-  c4mpool_alloc(mpool, sizeof(type) * cnt)	\
-
-typedef struct c4ls c4mpool_prefix_t;
-
-struct c4mpool {
-  struct c4ls ptrs;
-};
+struct c4mpool { struct c4ls ptrs; };
 
 struct c4mpool *c4mpool_init(struct c4mpool *self);
 void c4mpool_free(struct c4mpool *self);
