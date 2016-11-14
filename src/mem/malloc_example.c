ae2c661b0cb28ed6abf5950e7e16c0a6dd21e6d6
codr4life <codr4life@gmail.com>
2016-11-14 20:59:55 +0100

    updated examples

--- src/mem/malloc_example.c
+++ src/mem/malloc_example.c
@@ -3,7 +3,7 @@
 #include "malloc_example.h"
 
 static void *acquire(struct c4malloc *self, size_t size) {
-  //struct c4my_malloc *m = C4PTROF(c4my_malloc, malloc, self);
+  //struct my_malloc *m = C4PTROF(my_malloc, malloc, self);
 
   // Implement your algorithm here
   
@@ -11,13 +11,13 @@ static void *acquire(struct c4malloc *self, size_t size) {
 }
 
 static void release(struct c4malloc *self, void *ptr) {
-  //struct c4my_malloc *m = C4PTROF(c4my_malloc, malloc, self);
+  //struct my_malloc *m = C4PTROF(my_malloc, malloc, self);
 
   // Implementing release is optional,
   // as long as all memory is deallocated with the allocator
 }
 
-struct c4my_malloc *c4my_malloc_init(struct c4my_malloc *self) {
+struct my_malloc *my_malloc_init(struct my_malloc *self) {
   c4malloc_init(&self->malloc);
   self->malloc.acquire = acquire;
   self->malloc.release = release;
@@ -27,7 +27,7 @@ struct c4my_malloc *c4my_malloc_init(struct c4my_malloc *self) {
   return self;
 }
 
-void c4my_malloc_free(struct c4my_malloc *self) {
+void my_malloc_free(struct my_malloc *self) {
   c4malloc_free(&self->malloc);
 
   // Free any memory allocated from the heap here
--- src/mem/malloc_example.h
+++ src/mem/malloc_example.h
@@ -1,5 +1,5 @@
-#ifndef C4MALLOC_EXAMPLE_H
-#define C4MALLOC_EXAMPLE_H
+#ifndef MY_MALLOC_H
+#define MY_MALLOC_H
 
 // Include the c4malloc interface
 
@@ -7,23 +7,23 @@
 
 // A convenience macro to declare and initialize in one call
 
-#define C4MY_MALLOC(var)				\
-  struct c4my_malloc var;				\
-  c4my_malloc_init(&var);				\
+#define MY_MALLOC(var)				\
+  struct my_malloc var;				\
+  my_malloc_init(&var);				\
 
 // The struct containing the allocator state.
 // All implementations are required to implement the c4malloc interface.
 
-struct c4my_malloc {
+struct my_malloc {
   struct c4malloc malloc;
 
   // Add your allocator state here
 };
 
 // Initializes the internal state of the allocator
-struct c4my_alloc *c4my_alloc_init(struct c4my_alloc *self);
+struct my_alloc *my_alloc_init(struct my_malloc *self);
 
 // Frees the internal state of the allocator
-void c4my_alloc_free(struct c4my_alloc *self);
+void my_alloc_free(struct my_malloc *self);
 
 #endif
